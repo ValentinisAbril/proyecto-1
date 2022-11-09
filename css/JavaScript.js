d98242eb4cc8d3486a901fd62abd08e7a272b1df
ValentinisAbril <valentinisabril0gmail.com>
2022-11-09 15:58:35 -0300

    generando variables en js

---
+++ css/JavaScript.js
@@ -0,0 +1,4 @@
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
