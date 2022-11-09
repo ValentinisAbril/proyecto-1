37636ce77c99dc1ab815bcc49daec63df056cba2
ValentinisAbril <valentinisabril0gmail.com>
2022-11-09 16:02:34 -0300

    generando cambios

--- css/JavaScript.js
+++
@@ -1,4 +0,0 @@
-var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
-	overlay = document.getElementById('overlay'),
-	popup = document.getElementById('popup'),
-	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
