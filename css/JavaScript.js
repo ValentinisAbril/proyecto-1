04f7d95154417ab3f68daa0f6ce790351630f70d
ValentinisAbril <valentinisabril0gmail.com>
2022-11-09 23:08:16 -0300

    cambiando lineas de codigo en html

--- css/JavaScript.js
+++ css/JavaScript.js
@@ -3,13 +3,13 @@ var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
 	popup = document.getElementById('popup'),
 	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
 
-    btnAbrirPopup.addEventListener('click', function(){
-        overlay.classList.add('active');
-        popup.classList.add('active');
-    });
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
 
-    btnCerrarPopup.addEventListener('click', function(e){
-        e.preventDefault();
-        overlay.classList.remove('active');
-        popup.classList.remove('active');
-    });
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
